302f114196a13d7789f3366d86d0dcae3668d6c4
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-22 19:59:23 -0300

    teste

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-pipenv install
-python3 manage.py collectstatic --noinput
+
+python3.10 -m 
+ensurepip python3.10 -m 
+pip install -r requirements.txt 
+python3.10 manage.py collectstatic --noinput --clear echo "BUILD END"
