d394f2c0748f7cf9364cdf81687a442329d2f424
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-22 18:24:12 -0300

    added environment configuration files

--- quizz_django/settings.py
+++ quizz_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l2eo=jr+&dw0x=6&(r9(dk2$u$(n$$i69mblu!#j(2d5))zs8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
