5520e6b7749fd48f58eddc4d9b75d5ba01fe8402
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-22 19:49:19 -0300

    dasdsa

--- quizz_django/settings.py
+++ quizz_django/settings.py
@@ -121,17 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'waste/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
+MEDIA_URL= os.path.join(BASE_DIR, 'waste', "static", "waste", "sprites")
+MEDIA_ROOT= BASE_DIR/'media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'waste', "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'waste_build', 'static')
